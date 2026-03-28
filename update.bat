c88576f2c588815dad4b48cdbb4da570eedd26bc
klayza <clayjay9870@gmail.com>
2026-03-28 16:52:43 -0700

    remove scripts from prod

--- update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Updating Extension...
-
-pyrevit extensions delete Arie
-pyrevit extend ui ArieProd https://github.com/klayza/Arie.git --dest "%appdata%\pyRevit\Extensions" --branch prod --debug
-
-echo Done.
-pause
